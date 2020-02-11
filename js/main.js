bf1bca8d0754fcfa74f0527c5741e9705bbde0d7
danielj-francisco <johnd.francisco1@gmail.com>
2020-02-11 15:41:05 -0800

    Truth and Dare lists done

---
+++ js/main.js
@@ -0,0 +1,45 @@
+console.log("You got this, don't worry");
+
+// make the array for truths
+const truthList = [
+    "Have you ever ghosted someone on tinder?",
+    "Worst tinder date in detail",
+    "Have you ever hooked up with a tinder date",
+    "Do you take tinder seriously? Why or why not?",
+    "Message your last tinder match in deatil why your last relationship didn't work",
+    "Message a tinder match with the first letter 'A' your biggest insecurity",
+    "Describe in detail what your type is",
+    "Admit your biggest mistake to your opponent and a match on tinder",
+    "Admit why you wouldn't date youself and that most people don't learn until they date you",
+    "Explain why you chose each picture on your profile and who you are hoping to attract"
+]
+
+// make the array for dares
+const dareList = [
+    "Swipe right on the next 5 people",
+    "Let the other player swipe the next 10 people",
+    "Let the other player choose a pick-up line and send it to your last match",
+    "Message your last match that you want to meet their family",
+    "Give your number to your last 5 matches",
+    "Go to one of your matches' instagram and like the last 10 posts",
+    "Message any 5 of your matches saying 'asking why they slept with your ex?'",
+    "Ask your last match if they will go on a date with you",
+    "Change your picture on tinder to an embarrassing one",
+    "Message 3 matches saying you only made a tinder to piss off your boyfriend",
+    "Swipe right for the next 30 seconds and if they message, you have to hold a conversation with them"
+]
+
+// make the functions to call the Truths in random
+
+// make the functions to call the Dares in random
+
+// manipulate teh DOM for user interactions
+    // truth button
+    // dare button
+    // random button
+    // start button
+    // did it button
+    // nope button
+    // 
+
+//
