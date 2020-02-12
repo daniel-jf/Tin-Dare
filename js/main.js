d6d9d714831e7f3791ec597ac98369b65c1dc180
danielj-francisco <johnd.francisco1@gmail.com>
2020-02-11 19:45:21 -0800

    functions for a random truth or dare to appear in console created

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-console.log("You got this, don't worry");
+//console.log("You got this, don't worry");
 
 // make the array for truths
 const truthList = [
-    "Have you ever ghosted someone on tinder?",
-    "Worst tinder date in detail",
-    "Have you ever hooked up with a tinder date",
-    "Do you take tinder seriously? Why or why not?",
-    "Message your last tinder match in deatil why your last relationship didn't work",
-    "Message a tinder match with the first letter 'A' your biggest insecurity",
-    "Describe in detail what your type is",
-    "Admit your biggest mistake to your opponent and a match on tinder",
-    "Admit why you wouldn't date youself and that most people don't learn until they date you",
-    "Explain why you chose each picture on your profile and who you are hoping to attract"
+    'Have you ever ghosted someone on tinder?',
+    'Worst tinder date in detail',
+    'Have you ever hooked up with a tinder date',
+    'Do you take tinder seriously? Why or why not?',
+    'Message your last tinder match in deatil why your last relationship didn not work',
+    'Message a tinder match with the first letter O your biggest insecurity',
+    'Describe in detail what your type is',
+    'Admit your biggest mistake to your opponent and a match on tinder',
+    'Admit why you would not date youself and that most people do not learn until they date you',
+    'Explain why you chose each picture on your profile and who you are hoping to attract'
 ]
 
 // make the array for dares
@@ -29,12 +29,29 @@ const dareList = [
     "Swipe right for the next 30 seconds and if they message, you have to hold a conversation with them"
 ]
 
-console.log(dareList[3]);
-console.log(truthList[0]);
+// FUNCTIONS 
 
 // make the functions to call the Truths in random
+function randomTruth() {
+    for (let tListIdx = truthList.length -1; tListIdx >0; tListIdx--){
+    return truthList[Math.floor(Math.random() * truthList.length)]
+    };
+}
+randomTruth();
+console.log(randomTruth(truthList));
 
 // make the functions to call the Dares in random
+randomDare = function() {
+    for (let dListIdx = dareList.length +1; dListIdx > 0; dListIdx--){
+      return dareList[Math.floor(Math.random() * dareList.length)]
+    };
+}
+randomDare();
+console.log(randomDare(dareList));
+
+
+// make a function that will choose a random truth or dare
+
 
 // manipulate teh DOM for user interactions
     // truth button
@@ -43,7 +60,9 @@ console.log(truthList[0]);
     // start button
     // did it button
     // nope button
-    // 
+
 
 // create a function that will show when a player wins
+
+
 // make a function to show points earned 
